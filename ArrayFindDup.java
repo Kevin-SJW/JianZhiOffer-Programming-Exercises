a1acb383d911c37a97e88ad1486c25de1a9bd52e
JianWei Shi <JianWei Shi>
2019-02-20 18:54:38 +0800

    ArrayFindDup

--- ArrayFindDup.java
+++ ArrayFindDup.java
@@ -1,22 +1,33 @@
 package JianZhiOffer;
 
 import java.util.*;
+/*找出数组中唯一重复的数*/
+/*异或法，根据异或的法则，每两个相异的数执行异或运算后，
+ * 结果为1，每两个相同的数执行异或运算之后，结果为0；*/
 public class ArrayFindDup {
 	public static int ArrayFindDup(int[] arr){
-		int tmp1=0;
-		int tmp2=0;
 		int n=arr.length;
-		for(int i=0;i<n-1;i++){
-			tmp1+=(i+1);
-			tmp2+=arr[i];
+//		int tmp1=0;
+//		int tmp2=0;
+//		for(int i=0;i<n-1;i++){
+//			tmp1+=(i+1);
+//			tmp2+=arr[i];
+//		}
+//		tmp2+=arr[n-1];
+//		int result=tmp2-tmp1;
+//		return result;
+		int result=0;
+		for(int i=0;i<n;i++){
+			result^=arr[i];
 		}
-		tmp2+=arr[n-1];
-		int result=tmp2-tmp1;
+		for(int i=1;i<n;i++){
+			result^=arr[i];
+		}
 		return result;
 	
 	}
 	public static void main(String[] args){
-		int[] arr={1,2,1,3,4};
+		int[] arr={1,2,1,3,4,8,9,10};
 		int uniDup=ArrayFindDup(arr);
 		System.out.println("数组中唯一重复的数为：");
 		System.out.println(uniDup);
