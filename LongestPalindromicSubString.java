a4959c2c623b3005bb2766c30608dc472f1a2cd6
JianWei Shi <JianWei Shi>
2019-02-24 16:26:24 +0800

    LongestPalindromicSubString

--- LongestPalindromicSubString.java
+++ LongestPalindromicSubString.java
@@ -12,40 +12,36 @@ public class LongestPalindromicSubString {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(longestPalindrome1("babcbabcbaccba"));
+		System.out.println(longestPalindrome("babcbabcbaccba"));
 	}
 
-	public static String longestPalindrome1(String s) {
-
-		int maxPalinLength = 0;
+	public static String longestPalindrome(String s){
+		int maxPalinLen=0;
 		String longestPalindrome = null;
-		int length = s.length();
-
-		// check all possible sub strings
-		for (int i = 0; i < length; i++) {
-			for (int j = i + 1; j < length; j++) {
-				int len = j - i;
-				String curr = s.substring(i, j + 1);
-				if (isPalindrome(curr)) {
-					if (len > maxPalinLength) {
-						longestPalindrome = curr;
-						maxPalinLength = len;
-					}
+		//check all possible string;
+		for(int i=0;i<s.length();i++){
+			for(int j=i+1;j<s.length();j++){
+				int len=j-i;
+				String curr=s.substring(i, j+1);
+				if(isPalinDrome(curr)){
+					if(len>maxPalinLen)
+						longestPalindrome=curr;
+						maxPalinLen=len;
 				}
+				
 			}
 		}
-
 		return longestPalindrome;
+		 
+		
 	}
-
-	public static boolean isPalindrome(String s) {
-
-		for (int i = 0; i < s.length() - 1; i++) {
-			if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+	public static boolean isPalinDrome(String s){
+		for(int i=0;i<s.length()-1;i++){
+			if(s.charAt(i)!=s.charAt(s.length()-1-i)){
 				return false;
 			}
 		}
-
 		return true;
 	}
+	
 }
