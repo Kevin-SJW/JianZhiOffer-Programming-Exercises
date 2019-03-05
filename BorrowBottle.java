1b6cfa3999dfbe90cf9b0c1ce56a5e39ed1aafbe
JianWei Shi <JianWei Shi>
2019-03-05 10:03:03 +0800

    Borrow Bottle

---
+++ BorrowBottle.java
@@ -0,0 +1,23 @@
+package JianZhiOffer;
+
+import java.util.*;
+public class BorrowBottle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+        int a =sc.nextInt();
+        int count=0;
+        if(a==0)
+            return;
+        else if(a==1)
+            System.out.println(0);
+        else if(a==2)
+            System.out.println(1);
+        else{
+            count=a/2;
+            System.out.println(count);
+        }
+        
+        }
+    }
+}
