26e9db0462e8c5f173598a66522582b47c93da0c
JianWei Shi <JianWei Shi>
2019-03-10 15:28:19 +0800

    StringAppearOneTime

--- StringAppearOneTime.java
+++ StringAppearOneTime.java
@@ -1,39 +1,33 @@
 package JianZhiOffer;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Scanner;
 
-public class StringAppearOneTime {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-            HashMap<Character, Integer> map = new HashMap<Character,Integer>();
-            String str=sc.nextLine();
-            for(int i=str.length()-1;i>=0;i--){
-                if(map.containsKey(str.charAt(i))){
-                    map.put(str.charAt(i), map.get(str.charAt(i))+1);
-                }else{
-                    map.put(str.charAt(i), 1);
-                }
-                    
-            }
-            boolean flag =true;
-            Iterator<Entry<Character, Integer>> iter =map.entrySet().iterator();
-            for(;iter.hasNext();){
-                Entry<Character, Integer> entry=iter.next();
-                if(entry.getValue()==1){
-                    flag=false;
-                    System.out.println(entry.getKey());
-                    break;
-                }
-            }
-            if(flag){
-                System.out.println("null");
-            }
-    
+import java.util.*;
+;public class StringAppearOneTime {
+	public static void main(String[] args) {
 
-    }
-
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		LinkedHashMap<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+		for (int i = str.length() - 1; i >= 0; i--) {
+			if (map.containsKey(str.charAt(i))) {
+				map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+			} else {
+				map.put(str.charAt(i), 1);
+			}
+		}
+		boolean flag = true;
+		for(Map.Entry<Character, Integer> entry:map.entrySet()){
+			if(entry.getValue()==1){
+				flag=false;
+				System.out.println(entry.getKey());
+				break;
+			}
+			
+		}
+		if(flag){
+			System.out.println("NULL");
+		}
+		
+		
+	}
 }
